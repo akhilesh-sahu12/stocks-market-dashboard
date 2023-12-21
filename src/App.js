4dda4e284dd0db48781bf7ea7d7c430c7e7ad05f
akhilesh-sahu12 <akhilesh.jamus@gmail.com>
2023-12-22 00:13:23 +0530

    add dashboard, routing && using some dummy data to show charts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Dashboard/Header';
+import Sidebar from './components/Dashboard/Sidebar';
+import Home from './components/Home';
+import PageNotFound from './components/PageNotFound';
+import Login from './components/Auth/Login';
+import Registration from './components/Auth/Registration';
 
 function App() {
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  }
+
   return (
-    <>
-    </>
-  );
+    <Router>
+    <div className='grid-container'>
+      <Header OpenSidebar={OpenSidebar}/>
+      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Registration />} />
+          <Route path="/login" element={<Login />} />
+          <Route path='*' element={<PageNotFound/>}/>
+      </Routes>
+    </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
